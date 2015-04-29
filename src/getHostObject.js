b24f8608f89f0768d307746793c2ad293241a498
Erik Griffin <griffin.erik@gmail.com>
2015-04-30 00:48:30 +0100

    Somehow working with waterfall

--- src/getHostObject.js
+++ src/getHostObject.js
@@ -1,4 +1,4 @@
-// (function() {
+(function() {
 
   var async = require('async');
 
@@ -15,12 +15,10 @@
       hostObject.tracklist.push(song);
     };
 
-    var getSongs = function() {
+    var getSongs = function(cb) {
       redis.lrange(hostName+":songs",0,-1, function(err,data) {
         if (err) {console.log('Error reading songs! '+hostName+'\n'+err);}
-        for(var i=0;i<data.length;i++) {
-          redis.hgetall(hostName+":song:"+data[i], getSongObject);
-        }
+        cb(null,data);
       });
     };
 
@@ -29,32 +27,44 @@
       hostObject.pushers.push(pusher);
     };
 
-    var getPushers = function() {
+    var getPushers = function(cb) {
       redis.smembers(hostName+":pushers", function(err, data) {
         if (err) {console.log('Error reading pushers! '+hostName+'\n'+err);}
-        for(var i=0;i<data.length;i++) {
-          redis.hgetall(hostName+":pusher:"+data[i], getPusherObject);
-        }
+        cb(null, data);
       });
     };
 
-    async.parallel([
-      function(callback) {
-        redis.smembers(hostName+":pushers", function(err,data) {
-          if (err) {console.log('Erroar.');}
-          for (var i=0;i<data.length;i++) {
-            console.log('I dont get it');
-          }
+    async.waterfall([
+
+      function(cb) {
+        redis.smembers(hostName+":pushers", function(err, data) {
+          if (err) {console.log('Error reading pushers! '+hostName+'\n'+err);}
+          cb(null,data);
         });
       },
-      function(callback) {
-        getSongs();
+      function(pushers, cb) {
+        var temp = [];
+        for(var i=0;i<pushers.length;i++) {
+          temp.push((function(pusher){
+            return function(callback){
+              redis.hgetall(hostName+":pusher:"+pusher, callback);
+            };
+          })(pushers[i]));
+        }
+        async.parallel(temp, function(err, res) {
+          console.log('where am I?');
+          console.log(res);
+        });
       }
-    ],callback(hostObject));
+    ],function(err, result){
+      console.log('Maybe?');
+      console.log(result);
+    });
+
 
   };
 
 
   module.exports = getHostObject;
 
-// }());
+}());
