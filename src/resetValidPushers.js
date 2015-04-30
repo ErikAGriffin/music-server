eae07ef43f66e963dcf0124bfd5ba44caf785220
Erik Griffin <griffin.erik@gmail.com>
2015-04-30 14:21:39 +0100

    Refactored resetValidPushers:untested

--- src/resetValidPushers.js
+++ src/resetValidPushers.js
@@ -1,15 +1,32 @@
 (function() {
 
-  var async = require('async');
+//   var async = require('async');
 
-  var resetValidPushers = function(redis, update) {
+  var resetValidPushers = function(redis, hostName) {
 
     // takes hostName
 
     // currently just resetting for ALL pushers.
 
+    redis.smembers(hostName+":pushers", function(err, data) {
+      for(var i=0;i<pushers.length;i++) {
+        redis.hset(hostName+":pusher:"+pusher[i],'played',false);
+      }
+    });
 
 
+//     var getPushers = function(cb) {
+//       redis.smembers(hostName+":pushers", cb);
+//     };
+
+//     async.waterfall([
+//       getPushers,
+//       function(pushers, cb) {
+//         for(var i=0;i<pushers.length;i++) {
+//           redis.hset(hostName+":pusher:"+pusher[i],'played',false);
+//         }
+//       }
+//     ]);
 
   };
 
