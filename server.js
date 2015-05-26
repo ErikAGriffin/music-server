17739baf838270c7f0c8d480e02fe59c58634119
Erik Griffin <griffin.erik@gmail.com>
2015-05-26 15:20:55 +0200

    Cleaned house

--- server.js
+++ server.js
@@ -112,43 +112,6 @@
   });
 
 
-  // --- User Management ---
-
-  app.get('/login', function(req, res) {
-    var sess = req.session;
-    res.render('login', {user:sess.user});
-  });
-
-  app.post('/createuser', function(req,res) {
-    var sess = req.session;
-
-    bcrypt.createUser(req.body.email, req.body.password, function(user) {
-      db.users.insert(user, function(err,docs) {
-        if (err) {return console.error(err);}
-        console.log('Stored user');
-      });
-    });
-    // What should the session store? Something more secure?
-    // Can users create their own cookies?
-    sess.user = req.body.email;
-    res.redirect('/');
-  });
-
-  app.post('/checkunique/:email', function(req,res) {
-
-    db.users.findOne({email: req.params.email}, function(err,doc) {
-      if (err) {console.log(err);}
-      else if (doc) {
-        res.json({unique: false});
-      }
-      else {
-        res.json({unique: true});
-      }
-    });
-    console.log("Queried "+req.params.email);
-  });
-
-
   // Where is this used?
   module.exports = server;
 }());
